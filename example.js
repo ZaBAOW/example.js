2e13d2dc901c0924087e39d799b586119241dc44
Andrew Kadesky <amkadesky@aol.com>
2017-10-03 14:50:30 -0500

    example.js bot now works.

--- example.js
+++ example.js
@@ -1,10 +1,26 @@
+const Eris = require("eris");
+
 var bot = new Eris("MzYzNjk1OTY2ODI3NzA4NDM4.DLFDVg.NdwAuP6aZslSpGDG3Z5HAAuD3Go");
-bot.on("ready", () => {
-    console.log("Ready!");
+// Replace BOT_TOKEN with your bot account's token
+
+bot.on("ready", () => { // When the bot is ready
+    console.log("Ready!"); // Log "Ready!"
 });
-bot.on("messageCreate", (msg) => {
-    if(msg.content === "!ping") {
+
+bot.on("messageCreate", (msg) => { // When a message is created
+    if(msg.content === "!ping") { // If the message content is "!ping"
         bot.createMessage(msg.channel.id, "Pong!");
+        // Send a message in the same channel with "Pong!"
+    } else if(msg.content === "!pong") { // Otherwise, if the message is "!pong"
+        bot.createMessage(msg.channel.id, "Ping!");
+        // Respond with "Ping!"
     }
 });
-bot.connect();
+
+bot.on("messageCreate", (msg) => {
+	if(msg.content === "!weather") {
+		bot.createMessage(msg.channel.id, "The weather is nice");
+	}
+});
+
+bot.connect(); // Get the bot to connect to Discord
